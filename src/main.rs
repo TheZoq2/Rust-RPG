527dd1be3aff6ac235d3abf0a79b80dd3715c3e9
TheZoq2 <frans.skarman@gmail.com>
2016-02-07 15:27:01 +0100

    Work on weapon generation

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,93 @@ extern crate rand;
 
 enum DamageTypes
 {
-    Blunt(f32),
-    Sharp(f32),
-    Magic(f32),
-    Fire(f32),
-    Holy(f32)
+    Blunt,
+    Sharp,
+    Burning,
+    Poison,
+    Radiation,
+    END
 }
 
 struct ItemTrait
 {
-    name: &'static str,
-    modifiers: Vec<DamageTypes>
+    name: String,
+    weapon_modifiers: Vec<f32>
+}
+impl ItemTrait
+{
+    pub fn new(name: String, mods: Vec<(DamageTypes, f32)>) -> ItemTrait
+    {
+        let mut result = ItemTrait{name: name, weapon_modifiers: vec![1.0; DamageTypes::END as usize]};
+
+        result.setWeaponModifiers(mods);
+
+        result
+    }
+
+    pub fn setWeaponModifiers(&mut self, mods: Vec<(DamageTypes, f32)>)
+    {
+        for modifier in mods
+        {
+            self.weapon_modifiers[modifier.0 as usize] = modifier.1;
+        }
+    }
 }
 
+enum WeaponType
+{
+    melee,
+    bullet,
+    arrow,
+}
 struct BaseWeapon
 {
-    name: &'static str,
-    damage: Vec<DamageTypes>,
-    ranged: bool
+    name: String,
+    damage: Vec<f32>,
+    weapon_type: WeaponType
+}
+impl BaseWeapon
+{
+    pub fn new(name: String, weapon_type: WeaponType, dmgs: Vec<(DamageTypes, f32)>) -> BaseWeapon
+    {
+        let mut result = BaseWeapon{name: name, damage: vec![0.0; DamageTypes::END as usize], weapon_type: weapon_type};
+
+        result.setDamages(dmgs);
+
+        result
+    }
+
+    pub fn setDamages(&mut self, dmgs: Vec<(DamageTypes, f32)>)
+    {
+        for dmg in dmgs
+        {
+            self.damage[dmg.0 as usize] = dmg.1;
+        }
+    }
+
+    pub fn setDamage(&mut self, dmg_type: DamageTypes, value: f32)
+    {
+        self.damage[dmg_type as usize] = value;
+    }
 }
 
 struct Weapon
 {
     //base: BaseWeapon,
     name: String,
-    damage: Vec<DamageTypes>,
+    damage: Vec<f32>,
+}
+impl Weapon
+{
+    pub fn new(name: String) -> Weapon
+    {
+        Weapon{name: name.to_string(), damage: vec![0.0; DamageTypes::END as usize]}
+    }
+    
+    fn setDamage(&mut self, dmg_type: DamageTypes, value: f32)
+    {
+        self.damage[dmg_type as usize] = value;
+    }
 }
 
 struct GameParameters
@@ -43,24 +105,43 @@ fn setup_damage_traits() -> Vec<ItemTrait>
 {
     let mut damage_traits: Vec<ItemTrait> = vec![];
 
-    damage_traits.push(ItemTrait{ name: "godly", modifiers: vec![DamageTypes::Holy(2.0)]});
+    //damage_traits.push(DamageTrait::new("Godly", vec![]));
 
-    damage_traits.push(ItemTrait{ name: "broken", modifiers: vec![
-            DamageTypes::Blunt(0.5),
-            DamageTypes::Sharp(0.5),
-            DamageTypes::Magic(0.5)
-        ]});
+    damage_traits.push(ItemTrait::new("Masterpiece".to_string(), vec![
+        (DamageTypes::Sharp, 2.0),
+        (DamageTypes::Blunt, 1.5),
+        (DamageTypes::Burning, 1.6),
+        (DamageTypes::Poison, 2.0),
+        (DamageTypes::Radiation, 1.5),
+    ]));
 
-    damage_traits.push(ItemTrait{ name: "rusty", modifiers: vec![
-            DamageTypes::Blunt(0.5),
-            DamageTypes::Sharp(0.5),
-        ]});
-
-    damage_traits.push(ItemTrait{ name: "fine", modifiers: vec![
-            DamageTypes::Blunt(1.2),
-            DamageTypes::Sharp(1.2),
-            DamageTypes::Magic(1.2),
-        ]});
+    damage_traits.push(ItemTrait::new("polished".to_string(), vec![
+        (DamageTypes::Sharp, 1.5),
+        (DamageTypes::Burning, 0.8),
+        (DamageTypes::Poison, 1.3),
+        (DamageTypes::Radiation, 1.2),
+    ]));
+    damage_traits.push(ItemTrait::new("broken".to_string(), vec![
+        (DamageTypes::Sharp, 0.4),
+        (DamageTypes::Blunt, 0.8),
+    ]));
+    damage_traits.push(ItemTrait::new("corroded".to_string(), vec![
+        (DamageTypes::Sharp, 0.6),
+        (DamageTypes::Blunt, 0.5),
+        (DamageTypes::Radiation, 0.4),
+        (DamageTypes::Poison, 1.2)
+    ]));
+    damage_traits.push(ItemTrait::new("fine".to_string(), vec![
+        (DamageTypes::Sharp, 1.2),
+        (DamageTypes::Blunt, 1.2),
+        (DamageTypes::Radiation, 1.2),
+        (DamageTypes::Poison, 1.2),
+        (DamageTypes::Burning, 1.2)
+    ]));
+    damage_traits.push(ItemTrait::new("bent".to_string(), vec![
+        (DamageTypes::Sharp, 0.8),
+        (DamageTypes::Blunt, 0.9),
+    ]));
 
     damage_traits //return the new traits
 }
@@ -68,54 +149,66 @@ fn setup_weapons() -> Vec<BaseWeapon>
 {
     let mut weapons: Vec<BaseWeapon> = vec![];
 
-    weapons.push(BaseWeapon{ name: "axe", ranged: false, damage: vec![
-                DamageTypes::Blunt(0.7),
-                DamageTypes::Sharp(1.2),
-                DamageTypes::Magic(0.25),
-                DamageTypes::Holy(0.25)
-            ]}
-        );
-    weapons.push(BaseWeapon{ name: "shortsword", ranged: false, damage: vec![
-                DamageTypes::Blunt(0.2),
-                DamageTypes::Sharp(1.5),
-                DamageTypes::Magic(0.25),
-                DamageTypes::Holy(0.25)
-            ]}
-        );
-    weapons.push(BaseWeapon{ name: "torch", ranged: false, damage: vec![
-                DamageTypes::Blunt(0.6),
-                DamageTypes::Magic(0.25),
-                DamageTypes::Fire(2.0),
-                DamageTypes::Holy(0.25)
-            ]}
-        );
+    weapons.push(BaseWeapon::new("axe".to_string(), WeaponType::melee, vec![
+                (DamageTypes::Sharp, 1.2),
+                (DamageTypes::Blunt, 1.4),
+            ]));
 
+    weapons.push(BaseWeapon::new("shortsword".to_string(), WeaponType::melee, vec![
+                (DamageTypes::Sharp, 1.2),
+                (DamageTypes::Blunt, 1.4),
+            ]));
+    weapons.push(BaseWeapon::new("dagger".to_string(), WeaponType::melee, vec![
+                (DamageTypes::Sharp, 1.1),
+                (DamageTypes::Poison, 1.3),
+    ]));
+    weapons.push(BaseWeapon::new("greatsword".to_string(), WeaponType::melee, vec![
+                (DamageTypes::Sharp, 1.8),
+                (DamageTypes::Blunt, 0.7)
+    ]));
+    weapons.push(BaseWeapon::new("pistol".to_string(), WeaponType::bullet, vec![
+                (DamageTypes::Blunt, 1.0)
+    ]));
 
     weapons
 }
 
 fn generate_weapon(parameters: GameParameters) -> Weapon
 {
+    const MAX_TRAITS: usize =  2;
+
     let mut rng = rand::thread_rng();
 
     //Select a possible trait
-    let weapon_trait = rng.choose(&parameters.damage_traits).unwrap();
+    let trait_amount = rng.gen_range(0, MAX_TRAITS + 1);
+    let traits = rand::sample(&mut rng, &parameters.damage_traits, trait_amount);
 
     //Select a base
     let base = rng.choose(&parameters.base_weapons).unwrap();
 
-    let weapon_name = weapon_trait.name.to_string() + base.name;
+    //Generate the name of the new weapon
+    let mut weapon_name = "".to_string();
+    
+    for t in traits
+    {
+        weapon_name = weapon_name + " " + &t.name;
+    }
+    weapon_name = weapon_name + " " + &base.name;
 
     println!("{}", weapon_name);
+    let mut result = Weapon::new(weapon_name);
 
-    Weapon{name: weapon_name, damage: vec![]}
+    result
 }
 
 fn main() 
 {
     let damage_traits = setup_damage_traits();
-    let weapons = setup_damage_traits();
+    let weapons = setup_weapons();
 
+    let gameParams = GameParameters{damage_traits: damage_traits, base_weapons: weapons};
 
-    let GameParameters = 
+    generate_weapon(gameParams);
+
+    println!("Done");
 }
